9f2437e034ef10ea9c536bc4a9b362b077d49259
André Mendonça e Silva <dexmendoncadev@gmail.com>
2021-09-30 20:48:50 -0300

    FEATURE: Adicionado o custom parse format no plugin de date

--- src/app/plugins/datePlugin.ts
+++ src/app/plugins/datePlugin.ts
@@ -3,6 +3,9 @@ import dayjs from 'dayjs';
 import datefns from 'date-fns';
 import { FastifyPluginAsync } from 'fastify';
 
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+dayjs.extend(customParseFormat);
+
 const pluginFN = fp((ft: any, opts: FastifyPluginAsync, done:any) => {
 	ft.decorate('date', dayjs);
 	ft.decorate('datefns', datefns);
